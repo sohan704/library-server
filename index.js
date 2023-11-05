46341b5f6863f547c454f06fb847523482a37221
sohan704 <aridsohan005@gmail.com>
2023-11-05 13:33:32 +0600

    added new api endpoint for details route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,9 +30,51 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    
+
+    const bookCollection = client.db('bookDB').collection('books');
+
+    app.post('/book', async (req, res) => {
+      const newBook = req.body;
+      const result = await bookCollection.insertOne(newBook);
+      res.send(result);
+    })
+
+    app.get('/book', async (req, res) => {
+      const cursor = bookCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
 
+    app.get('/book/:category', async (req, res) => {
+      const category = req.params.category;
+      const query = { category: category };
+      const result = await bookCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.get('/book/:category/:id', async (req, res) => {
+      try {
+        const category = req.params.category;
+        const id = req.params.id;
+
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send('Invalid ID');
+        }
+
+        const query = { category: category, _id: new ObjectId(id) };
+        const result = await bookCollection.findOne(query);
+
+        if (!result) {
+          return res.status(404).send('Product not found');
+        }
+
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
 
 
     await client.db("admin").command({ ping: 1 });
